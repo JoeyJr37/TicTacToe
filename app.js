b4e6a51d09d93de7aaebb3ac8d5213f191b8c7b4
JoeyJr <JoeyJr11@gmail.com>
2019-12-27 06:26:44 -0600

    added Borders and placeMarker function

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const gameBoard = (()=> {
-    const internalGameBoard = ["x", "o", "o", 
-                                "x", "o", "x", 
-                                "o", "x", "o"];
+    const internalGameBoard = [];
     return {internalGameBoard};
 
 })();
@@ -9,24 +7,24 @@ const gameBoard = (()=> {
 const displayCtrl = ((gameArray) => {
     //1. generateBoard
     const UIboard = document.querySelector(".gameBoard");
-    const make = (element) => {
-        return document.createElement(element);
-    };
-    const createNewCell = (element, playerMarker) => {
-        let newCell = make(element);
-        newCell.textContent = playerMarker;
-        return newCell;
-    };
+
     const render = () => {
-        for (let i = 0; i < gameArray.length; i++){
-        let gameMarker = createNewCell("div", gameArray[i]);
-        gameMarker.setAttribute("id", i);   
+        let arrayLength = gameArray.length;
+        for (let i = 0; i < arrayLength; i++){
+        let gameMarker = document.getElementById(`grid-${i}`);
+        gameMarker.textContent = gameArray[i];   
         UIboard.append(gameMarker);
        };
     };
     //2. listen for clicks
+    // const playerClick = document.querySelector(".gameBoard");
+    // playerClick.addEventListener("click", placeMarker);
     //3. generate marker on game board from array
-    return {render};
+    const placeMarker = (event) => {
+        let gameSpot = event.target;
+        gameSpot.textContent = "X";
+    };
+    return {render, placeMarker};
 })(gameBoard.internalGameBoard);
 
 const Player = (playerNumber, marker, score) => {
@@ -34,11 +32,16 @@ const Player = (playerNumber, marker, score) => {
 };
 const gameFlow = ((UIctrl, boardCtrl) => {
   // 1. generateBoard
-  const gamingTable = boardCtrl.internalGameBoard;
+  // const gamingTable = boardCtrl.internalGameBoard;
   UIctrl.render();
     // 2. generatePlayers
+    const playerOne = Player(1, "X", 0);
+    const playerTwo = Player(2, "O", 0);
     // 3. setActivePlayer
+    
     // 4. listenForClicks
+    const playerClick = document.querySelector(".gameBoard");
+    playerClick.addEventListener("click", UIctrl.placeMarker);
     // 5. Fill array with marker from active player
     // 6. generate marker on game board from array [render]
     // 7. check for winner (if no winner and no tie; go back to step 3)
