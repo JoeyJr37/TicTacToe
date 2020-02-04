054d7f3650cea604ac54e080727507756f068ea4
JoeyJr <JoeyJr11@gmail.com>
2020-02-03 20:22:36 -0600

    Updated logic

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ const gameBoard = (()=> {
 const displayCtrl = ((gameArray) => {
     const UIboard = document.querySelector(".gameBoard");
     const twoPlayersBtn = document.querySelector("#twoPlayersBtn");
+    let activePlayer;
+    let playerOne = document.querySelector("#playerOne");
+    let playerTwo = document.querySelector("#playerTwo");
     const Player = (playerNumber, marker, activeStatus) => {
         return {playerNumber, marker, activeStatus };
     };
     const render = () => {
         let arrayLength = gameArray.length;
         for (let i = 0; i < arrayLength; i++){
-        let gameMarker = document.getElementById(`grid-${i}`);
-        gameMarker.textContent = gameArray[i];   
-        UIboard.append(gameMarker);
-       };
+            let gameMarker = document.getElementById(`grid-${i}`);
+            gameMarker.textContent = gameArray[i];   
+        };
     };
     const setActivePlayer = (one, two) => {
-        let activePlayer;
         if (one.activeStatus === true){
             activePlayer = one;
             console.log("It's Player 1's turn");
@@ -29,26 +30,39 @@ const displayCtrl = ((gameArray) => {
         };
         return activePlayer;
     };
+    const resetActivePlayer = (one, two) => {
+        if (one.activeStatus === true){
+            one.activeStatus = false;
+            two.activeStatus = true;
+            setActivePlayer(one, two);
+        } else {
+            one.activeStatus = true;
+            two.activeStatus = false;
+            setActivePlayer(one, two);
+        };
+    };
     const twoPlayerMode = () => {
+        let duelMode = "active";
         const playerTags = document.querySelectorAll(".player-tags");
         const playerTagsArray = Object.values(playerTags);
         playerTagsArray.forEach(item => item.style.display = "inline");
-        const playerOne = Player(1, "X", true);
-        const playerTwo = Player(2, "O", false);
+        playerOne = Player(1, "X", true);
+        playerTwo = Player(2, "O", false);
         setActivePlayer(playerOne, playerTwo);
-        return {playerOne, playerTwo}
+        return {duelMode, playerOne, playerTwo}
     };
     //3. generate marker on game board from array
     const placeMarker = (event) => {
         let gameSpot = event.target.id;
         let gameSpotArray = Array.from(gameSpot);
-        let gameSpotID = Number(gameSpotArray[5]) + 1;
+        let gameSpotID = Number(gameSpotArray[5]);
         gameArray[gameSpotID] = activePlayer.marker;
-        // put X or O in game array from click based on active player
-        // gameArray [grid-spot] = activePlayer.marker
         console.log(gameSpotID);
+        render();
+        resetActivePlayer(playerOne, playerTwo);
+        // gameArray[gameSpotID] = activePlayer.marker;
     };
-    return {render, placeMarker, twoPlayerMode};
+    return {twoPlayersBtn, render, placeMarker, twoPlayerMode, setActivePlayer};
 })(gameBoard.internalGameBoard);
 
 
@@ -57,11 +71,20 @@ const gameFlow = ((UIctrl, boardCtrl) => {
     // 1. generateBoard
     // UIctrl.render();
     // 2. generatePlayers
-    twoPlayersBtn.addEventListener("click", UIctrl.twoPlayerMode);
+    let duelMode = "inactive";
+    UIctrl.twoPlayersBtn.addEventListener("click", () => {
+        UIctrl.twoPlayerMode();
+        duelMode = "active";
+    });
     // 3. setActivePlayer
     // 4. listenForClicks
     const playerClick = document.querySelector(".gameBoard");
-    playerClick.addEventListener("click", UIctrl.placeMarker);
+    playerClick.addEventListener("click", (event)=> {
+        if (duelMode === "active") {
+                UIctrl.placeMarker(event);
+        } else {
+            console.log("Game is not on!");
+        };});
     // 5. Fill array with marker from active player
     // boardCtrl.internalGameBoard[i].textContent = activePlayer.marker;
     // 6. generate marker on game board from array [render]
